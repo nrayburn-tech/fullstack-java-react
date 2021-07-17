f4955929ea28ae29f427e5be2d876093bde2f3e9
Nick Rayburn <nrayburn@cybermemphis.com>
2021-07-17 10:03:28 -0500

    remove eslint-plugin-jsx-a11y

--- src/main/react/.eslintrc.js
+++ src/main/react/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
     'prettier'
   ],
   env: {
