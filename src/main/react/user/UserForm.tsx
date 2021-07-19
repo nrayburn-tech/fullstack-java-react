dd00bae0377c2fbfef5e569edf3e55df453f0111
Nick Rayburn <nrayburn@cybermemphis.com>
2021-07-19 18:48:48 -0500

    update hook dependencies for userform wrap updates into a timer that saves values 0.5 seconds after changing

--- src/main/react/user/UserForm.tsx
+++ src/main/react/user/UserForm.tsx
@@ -15,23 +15,26 @@ function UserForm() {
     getUser(null).then((data) => {
       form.setFieldsValue(data);
     });
-  }, []);
+  }, [form]);
 
-  const handleDataChange = useCallback((data) => {
-    if (!Array.isArray(data)) {
-      return;
-    }
-    clearTimeout(dataChangeTimeout);
-    dataChangeTimeout = window.setTimeout(() => {
-      const saveData = cloneDeep(form.getFieldsValue());
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].errors.length) {
-          unset(saveData, data[i].name);
-        }
+  const handleDataChange = useCallback(
+    (data) => {
+      if (!Array.isArray(data)) {
+        return;
       }
-      updateUser(saveData).then();
-    }, 500);
-  }, []);
+      clearTimeout(dataChangeTimeout);
+      dataChangeTimeout = window.setTimeout(() => {
+        const saveData = cloneDeep(form.getFieldsValue());
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].errors.length) {
+            unset(saveData, data[i].name);
+          }
+        }
+        updateUser(saveData).then();
+      }, 500);
+    },
+    [form]
+  );
 
   return (
     <Form<User> form={form} labelCol={labelCol} onFieldsChange={handleDataChange}>
