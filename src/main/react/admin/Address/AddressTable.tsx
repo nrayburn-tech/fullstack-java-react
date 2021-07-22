07d1e342fbdf469bc503751ede84a7e750fccdd1
Nick Rayburn <nrayburn@cybermemphis.com>
2021-07-21 19:13:02 -0500

    move columns outside of component scope

--- src/main/react/admin/Address/AddressTable.tsx
+++ src/main/react/admin/Address/AddressTable.tsx
@@ -1,42 +1,39 @@
 import type { ColumnProps } from 'antd/es/table';
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import AddressFormComp from '../../components/AddressForm';
 import TableWithForm from '../../components/TableWithForm';
 import type { Address } from '../../types';
 
+const addressColumns: ColumnProps<Address>[] = [
+  {
+    title: 'Line One',
+    dataIndex: 'lineOne'
+  },
+  {
+    title: 'Line Two',
+    dataIndex: 'lineTwo'
+  },
+  {
+    title: 'City',
+    dataIndex: 'city'
+  },
+  {
+    title: 'State',
+    dataIndex: 'state'
+  },
+  {
+    title: 'Zip',
+    dataIndex: 'zip'
+  }
+];
 export function AddressTable({ userId }: { userId?: number }) {
-  const columns: ColumnProps<Address>[] = useMemo(() => {
-    return [
-      {
-        title: 'Line One',
-        dataIndex: 'lineOne'
-      },
-      {
-        title: 'Line Two',
-        dataIndex: 'lineTwo'
-      },
-      {
-        title: 'City',
-        dataIndex: 'city'
-      },
-      {
-        title: 'State',
-        dataIndex: 'state'
-      },
-      {
-        title: 'Zip',
-        dataIndex: 'zip'
-      }
-    ];
-  }, []);
-
   return (
     <div>
       <TableWithForm
         url='/api/address'
         urlList={userId ? `/api/user/address/${userId}` : undefined}
-        columns={columns}
+        columns={addressColumns}
         modal={{ title: 'Address' }}
       >
         <AddressFormComp />
--- src/main/react/admin/User/UserTable.tsx
+++ src/main/react/admin/User/UserTable.tsx
@@ -1,28 +1,24 @@
 import type { ColumnProps } from 'antd/es/table';
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import TableWithForm from '../../components/TableWithForm';
 import UserFormComp from '../../components/UserForm';
 import type { User } from '../../types';
 
-const url = '/api/user';
+const userColumns: ColumnProps<User>[] = [
+  {
+    title: 'First Name',
+    dataIndex: 'firstName'
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName'
+  }
+];
 export function UserTable() {
-  const columns: ColumnProps<User>[] = useMemo(() => {
-    return [
-      {
-        title: 'First Name',
-        dataIndex: 'firstName'
-      },
-      {
-        title: 'Last Name',
-        dataIndex: 'lastName'
-      }
-    ];
-  }, []);
-
   return (
     <div>
-      <TableWithForm url={url} columns={columns} modal={{ title: 'User' }}>
+      <TableWithForm url={'/api/user'} columns={userColumns} modal={{ title: 'User' }}>
         <UserFormComp />
       </TableWithForm>
     </div>
