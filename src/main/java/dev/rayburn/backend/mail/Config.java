4f9e8c1793fe8268f0066e47a354b1af20d57b6f
Nick Rayburn <nrayburn@cybermemphis.com>
2021-12-02 23:22:23 -0600

    update mailservice and mailconfig

--- src/main/java/dev/rayburn/backend/mail/Config.java
+++ src/main/java/dev/rayburn/backend/mail/Config.java
@@ -1,10 +1,12 @@
 package dev.rayburn.backend.mail;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
@@ -14,49 +16,34 @@ import java.util.Properties;
 @Log4j2
 public class Config {
 
-    @Value("${spring.mail.host:#{null}}")
-    private String host;
-    @Value("${spring.mail.port:#{null}}")
-    private Integer port;
-    @Value("${spring.mail.username:#{null}}")
-    private String username;
-    @Value("${spring.mail.password:#{null}}")
-    private String password;
-//    @Value("${spring.mail.properties}:#{null}")
-//    private Properties properties;
+    @Autowired
+    @Qualifier("mailProperties")
+    private MailProperties properties;
 
     /**
      * Only activated if we have a host, username, and password.
-     *
      */
     @Bean
     @ConditionalOnProperty({"spring.mail.host", "spring.mail.username", "spring.mail.password"})
-    public JavaMailSender getJavaMailSender() {
+    public JavaMailSenderImpl getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        if (port != null){
-            mailSender.setPort(port);
-        }
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setHost(properties.getHost());
+        mailSender.setPort(properties.getPort());
+        mailSender.setUsername(properties.getUsername());
+        mailSender.setPassword(properties.getPassword());
+        mailSender.setProtocol(properties.getProtocol());
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-
-        //TODO: This isn't currently working.  Need to figure out how to use
-        // @Value to get all the properties under spring.mail.properties.*
-//        if (properties != null){
-//            for (Map.Entry<Object, Object> propEntry : properties.entrySet()){
-//                System.out.println(propEntry.getKey() + " " + propEntry.getValue());
-//                if (props.contains(propEntry.getKey())){
-//                    log.info("Overriding property for " + propEntry.getKey());
-//                }
-//                props.setProperty((String) propEntry.getKey(), (String) propEntry.getValue());
-//            }
-//        }
+        Properties props = new Properties();
+        props.putAll(properties.getProperties());
+        mailSender.setJavaMailProperties(props);
 
         return mailSender;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.mail")
+    @Qualifier("mailProperties")
+    public MailProperties mailProperties() {
+        return new MailProperties();
     }
 }
--- src/main/java/dev/rayburn/backend/mail/MailServiceImpl.java
+++ src/main/java/dev/rayburn/backend/mail/MailServiceImpl.java
@@ -1,37 +1,34 @@
 package dev.rayburn.backend.mail;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
-import org.springframework.mail.*;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class MailServiceImpl implements MailSender {
-
-    @Value("${spring.mail.username:#{null}}")
-    private String username;
-    @Value("${spring.mail.from:#{null}}")
-    private String from;
 
     /**
      * This is null if config values are not supplied
      * for {@link dev.rayburn.backend.mail.Config}
      */
-    private final JavaMailSender mailSender;
+    private final JavaMailSenderImpl mailSender;
 
     @Autowired
-    public MailServiceImpl(@Nullable JavaMailSender mailSender){
+    public MailServiceImpl(@Nullable JavaMailSenderImpl mailSender) {
         super();
         this.mailSender = mailSender;
     }
 
-    public void sendEmail(String to, String subject, String text){
-        if (mailSender != null){
+    public void sendEmail(String to, String subject, String text) {
+        if (mailSender != null) {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(from != null ? from : username);
             message.setTo(to);
             message.setSubject(subject);
             message.setText(text);
@@ -41,14 +38,14 @@ public class MailServiceImpl implements MailSender {
 
     @Override
     public void send(@NonNull SimpleMailMessage simpleMessage) throws MailException {
-        if (mailSender != null){
+        if (mailSender != null) {
             mailSender.send(simpleMessage);
         }
     }
 
     @Override
     public void send(@NonNull SimpleMailMessage... simpleMessages) throws MailException {
-        for (SimpleMailMessage message: simpleMessages){
+        for (SimpleMailMessage message : simpleMessages) {
             send(message);
         }
     }
