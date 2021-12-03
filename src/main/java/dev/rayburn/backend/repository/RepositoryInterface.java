3029fb0fbad3219d96c2af6987ca3d60b39fc591
Nick Rayburn <nrayburn@cybermemphis.com>
2021-12-03 00:18:04 -0600

    extend JpaRepository instead of CrudRepository

--- src/main/java/dev/rayburn/backend/repository/RepositoryInterface.java
+++ src/main/java/dev/rayburn/backend/repository/RepositoryInterface.java
@@ -1,9 +1,9 @@
 package dev.rayburn.backend.repository;
 
 import dev.rayburn.backend.entity.AbstractEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface RepositoryInterface<Entity extends AbstractEntity> extends CrudRepository<Entity, Long> {
+public interface RepositoryInterface<Entity extends AbstractEntity> extends JpaRepository<Entity, Long> {
 }
